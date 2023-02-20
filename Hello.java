13ef4024516215af9957a8e85cdb427073aec05f
MuturiIsaac <125857937+MuturiIsaac@users.noreply.github.com>
2023-02-20 10:26:23 +0300

    update

--- Hello.java
+++ Hello.java
@@ -3,8 +3,7 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello nimrod");
         // Change from both collaborators are kept
-        System.out.println("Welcome");
-        System.out.println("update 2.0");
+       
 
     }
 
